ea30c7694a13c2ae1000c357e0b42df391f797bf
zwiegespalten <74203280+zwiegespalten@users.noreply.github.com>
2025-01-18 16:44:01 +0100

    down_data.py will download all images from MAST with given filters

---
+++ down_data.py
@@ -0,0 +1,29 @@
+from astroquery.mast import Catalogs
+from astroquery.vizier import Vizier
+from astroquery.mast import Observations
+import urllib.request
+import os
+
+directory_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(directory_path)
+
+filter_wide = ["F555W","F606W"]
+for f in filter_wide:
+    if not os.path.isdir("data/" + f + "/"):
+        os.makedirs("data/" + f + "/")
+    obsTable = Observations.query_criteria(calib_level= 3, dataproduct_type = 'image',
+                                               obs_collection = ["HLA"],
+                                        instrument_name = ["WFC3/UVIS"], filters = [f])
+
+    for url in obsTable['dataURL']:
+        if isinstance(url, type(obsTable[7]['dataURL'])):  #to have valid url, example: https://hla.stsci.edu/cgi-bin/getdata.cgi?dataset=hst_11426_21_wfc3_uvis_f606w_drz.fits
+            if url[-5:] == str(".fits"):
+                file = "data/" + f + "/" + str(url[50:])
+                if not os.path.exists(file):
+                    urllib.request.urlretrieve(url,file)
+ 
+#This code will download all fits images with filters F555W and F606W. 
+#Example of valid url https://hla.stsci.edu/cgi-bin/getdata.cgi?dataset=hst_11426_21_wfc3_uvis_f606w_drz.fits 
+#To filter used data the dataset.txt can be used
+#If image appears more times in dataset.txt it means that image was cut into multiple images
+
