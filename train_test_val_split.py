f653373f6a3cdb410c1c334d7ba75f610ee15f00
zwiegespalten <74203280+zwiegespalten@users.noreply.github.com>
2025-01-18 16:24:30 +0100

    train_test_val_split.py divides the downloaded dataset into 4/1/1 train, test and validation

--- train_test_val_split.py
+++ train_test_val_split.py
@@ -6,13 +6,18 @@ directory = os.path.dirname(__file__)
 os.chdir(directory)
 
 data_directory = './data'
-destination = './original_data'
+destination = './original_dataset'
 destinations = [os.path.join(destination, d) for d in ['training', 'test', 'eval']]
 
 for dest in destinations:
     os.makedirs(dest, exist_ok=True)
 
-image_filepaths = [os.path.join(data_directory, f) for f in os.listdir(data_directory) if f.endswith('.fits')]
+image_filepaths = []
+for subdir in os.listdir(data_directory):
+    subdir_path = os.path.join(data_directory, subdir)
+    if os.path.isdir(subdir_path):
+        image_filepaths.extend([os.path.join(subdir_path, file) for file in os.listdir(subdir_path) if file.endswith('.fits')])
+
 random.shuffle(image_filepaths)
 
 ratios = (60, 80, 100)  # Ratios in percentage
@@ -27,3 +32,4 @@ for dest, (start, end) in zip(destinations, indices):
     images = image_filepaths[start:end]
     for image in images:
         shutil.copy(image, os.path.join(dest, os.path.basename(image)))
+        os.remove(image)
